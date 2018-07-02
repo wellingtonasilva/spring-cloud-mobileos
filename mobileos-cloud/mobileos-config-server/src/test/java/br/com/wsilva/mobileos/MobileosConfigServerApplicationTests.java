26d6db41b1c85c915c9d6fd9438121a3888cda3b
Wellington A. Silva <wellingtonasilva@gmail.com>
2018-07-01 22:21:02 -0400

    1.Ajuste na configuração do properties

---
+++ mobileos-cloud/mobileos-config-server/src/test/java/br/com/wsilva/mobileos/MobileosConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.wsilva.mobileos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MobileosConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
