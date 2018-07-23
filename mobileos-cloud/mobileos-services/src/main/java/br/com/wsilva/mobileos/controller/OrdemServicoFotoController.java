95630e4ee49a778daba47ed9080cb2a08a0c8bea
Wellington A. Silva <wellingtonasilva@gmail.com>
2018-07-23 12:01:12 -0400

    1.Adicionado metodo para upload e download de imagens capturadas em campo

--- mobileos-cloud/mobileos-services/src/main/java/br/com/wsilva/mobileos/controller/OrdemServicoFotoController.java
+++ mobileos-cloud/mobileos-services/src/main/java/br/com/wsilva/mobileos/controller/OrdemServicoFotoController.java
@@ -3,9 +3,12 @@ package br.com.wsilva.mobileos.controller;
 import br.com.wsilva.mobileos.entity.OrdemServicoFotoEntity;
 import br.com.wsilva.mobileos.repository.OrdemServicoFotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -38,5 +41,33 @@ public class OrdemServicoFotoController {
     public String delete(@PathVariable("id") Long id) {
         this.repository.delete(id);
         return HttpStatus.OK.toString();
+    }
+
+    @RequestMapping(value = "/download/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<byte[]> downloadImage(@PathVariable(name = "id") Long id) {
+        OrdemServicoFotoEntity entity = repository.findOne(id);
+        if (entity != null) {
+            return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                    "attachment; filename=\"" + entity.getNomeFoto() + "\"").body(entity.getFoto());
+        }
+
+        return null;
+    }
+
+    @PostMapping("/upload/{id}")
+    public String updateloadImage(@PathVariable(name = "id") Long id, @RequestParam("file") MultipartFile file) {
+        OrdemServicoFotoEntity entity = repository.findOne(id);
+        if (entity != null) {
+            try {
+                entity.setFoto(file.getBytes());
+                repository.save(entity);
+                return Boolean.TRUE.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return Boolean.FALSE.toString();
     }
 }
