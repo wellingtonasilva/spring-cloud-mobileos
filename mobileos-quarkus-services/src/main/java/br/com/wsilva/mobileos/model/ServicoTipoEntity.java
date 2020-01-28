33709fdf9b88a9f1a9fc85af38a3a744c4fc5c56
Wellington A. Silva <wellingtonasilva@gmail.com>
2020-01-28 22:03:03 +0000

    Added Project Lombok

--- mobileos-quarkus-services/src/main/java/br/com/wsilva/mobileos/model/ServicoTipoEntity.java
+++ mobileos-quarkus-services/src/main/java/br/com/wsilva/mobileos/model/ServicoTipoEntity.java
@@ -1,10 +1,11 @@
 package br.com.wsilva.mobileos.model;
 
+import lombok.Data;
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
 @Table(name = "mos_servico_tipo")
+@Data
 public class ServicoTipoEntity {
 
     @Id
@@ -14,34 +15,4 @@ public class ServicoTipoEntity {
 
     @Column(name = "svtp_dsservicotipo")
     private String descricao;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ServicoTipoEntity that = (ServicoTipoEntity) o;
-        return id == that.id &&
-                Objects.equals(descricao, that.descricao);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, descricao);
-    }
 }
